29637d1fdd651db1faf73833a71e83a4c5e94573
pierregirou <pierre.girou42290@gmail.com>
2020-02-27 17:19:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-xbrh7g
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-xbrh7g)
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertComponent } from './product-alert/product-alert.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
+
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'product/:producId', component: ProductDetailComponent},
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertComponent,
+    ProductDetailComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
